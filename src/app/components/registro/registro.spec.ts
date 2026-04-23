726408f6ce1692766db3163851bbaff34166d3dd
ludmila <luzmilavillegas31@gmail.com>
2026-04-23 16:53:28 -0300

    actualizacion

--- src/app/components/registro/registro.spec.ts
+++ src/app/components/registro/registro.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Registro } from './registro';
+import { RegistroComponent } from './registro';
 
 describe('Registro', () => {
-  let component: Registro;
-  let fixture: ComponentFixture<Registro>;
+  let component: RegistroComponent;
+  let fixture: ComponentFixture<RegistroComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Registro],
+      imports: [RegistroComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Registro);
+    fixture = TestBed.createComponent(RegistroComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });

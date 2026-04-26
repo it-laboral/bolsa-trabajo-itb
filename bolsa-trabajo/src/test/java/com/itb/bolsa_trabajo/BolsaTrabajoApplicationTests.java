84251513a4cb079afd3291d56a3e6083eba5c320
it-laboral <36067205@ITBELTRAN.COM.AR>
2026-04-25 21:08:17 -0300

    API REST con Spring Boot y MySQL

---
+++ bolsa-trabajo/src/test/java/com/itb/bolsa_trabajo/BolsaTrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.bolsa_trabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaTrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
